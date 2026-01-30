850dab61db55e02e2fe481362d5dcfe23209be69
Abdullah Al Hadi <abdullah.hadi@cefalo.com>
2026-01-30 22:22:31 +0600

    Create empty SwiftUI demo app

---
+++ LogHub-Demo/LogHub-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LogHub-Demo
+//
+//  Created by Abdullah Al Hadi on 30/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LogHub-Demo/LogHub-Demo/LogHub_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogHub_DemoApp.swift
+//  LogHub-Demo
+//
+//  Created by Abdullah Al Hadi on 30/1/26.
+//
+
+import SwiftUI
+
+@main
+struct LogHub_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
